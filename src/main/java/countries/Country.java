6550237acd5d20714a9868e7037da3bc61348864
Denisiooo <fursevich3@gmail.com>
2022-12-22 19:26:52 +0300

    first commit

---
+++ src/main/java/countries/Country.java
@@ -0,0 +1,34 @@
+package countries;
+
+public class Country {
+    private String name;
+    private String city;
+
+    public Country(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Country" +
+                "name='" + name + '\'' +
+                ", city='" + city;
+    }
+}
---
+++ src/main/java/sponsorship/Housing.java
@@ -0,0 +1,4 @@
+package sponsorship;
+
+public class Housing {
+}
